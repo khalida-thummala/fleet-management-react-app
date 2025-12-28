e6df7a67cc7603897507e69621726c7e11907edd
khalida-thummala <khalidathummala38@gmail.com>
2025-12-28 20:26:04 +0530

    updated image link

--- src/components/FleetCard.jsx
+++ src/components/FleetCard.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const FleetCard = React.memo(({ fleet, updateDriver, toggleAvailability, deleteFleet }) => {
   return (
     <div className="card">
-      <img src="https://coding-platform.s3.amazonaws.com/dev/lms/tickets/5e80fcb6-3f8e-480c-945b-30a5359eb40e/JNmYjkVr3WOjsrbu.png" alt="vehicle" />
+      <img src="https://coding-platform.s3.amazonaws.com/dev/lms/tickets/5e80fcb6-3f8e-480c-945b-30a5359eb40e/JNmYjkVr3WOjsrbu.png"
+       alt="vehicle"
+       className = "fleet-image" />
       <p><b>Reg:</b> {fleet.regNo}</p>
       <p><b>Driver:</b> {fleet.driver}</p>
       <p><b>Status:</b> {fleet.available ? "Available" : "Unavailable"}</p>
